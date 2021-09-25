7582664cafeecabd0a5052d76f4573e7d4151405
Alvaro Merino <amerino.informatica@gmail.com>
2021-09-25 19:17:09 +0200

    Install Meslo Fonts

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
 # WSL2 install scripts
-* wsl_install_script.ps1 --> Script to install WSL2.
+* wsl_install_script.ps1 --> Script to install WSL2 and fonts.
 * install_script.sh --> Script to install the essentials tools in a wsl2 ubuntu machine.
 
 # Instructions
 ## Powershell script
 ### Basic usage
-1. Copy the wsl_script.ps1 in your home directory.
-2. Run the following command to allow the execution of the script.
-```bash
-chmod u+x install_script.sh
-```
-3. Run the script with the following command.
-```
-./install_script.sh
-```
-4. Select your choice (The first one install every other one).
+1. Copy the wsl_script.ps1 in your desktop directory.
+2. Execute the script with powershell.
+3. The script will be executed with admin privileges.
+4. Select your choice.
 
 ## Bash script
 ### Basic usage
--- wsl_script.ps1
+++ wsl_script.ps1
@@ -21,10 +21,35 @@ function Update-WSL {
     Write-Host "`nWSL2 installed"
 }
 
+function Install-Fonts {
+    mkdir $DesktopPath\Fonts
+    Invoke-WebRequest -Uri "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf" -Outfile $DesktopPath\Fonts\MesloLGS_NF_Regular.ttf
+    Invoke-WebRequest -Uri "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf" -Outfile $DesktopPath\Fonts\MesloLGS_NF_Bold.ttf
+    Invoke-WebRequest -Uri "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf" -Outfile $DesktopPath\Fonts\MesloLGS_NF_Italic.ttf
+    Invoke-WebRequest -Uri "https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf" -Outfile $DesktopPath\Fonts\MesloLGS_NF_Bold_Italic.ttf
+    
+
+    $FontsFolder = "$DesktopPath\Fonts"
+    $FONTS = 0x14
+    $CopyOptions = 4 + 16;
+    $objShell = New-Object -ComObject Shell.Application
+    $objFolder = $objShell.Namespace($FONTS)
+    $allFonts = dir $FontsFolder
+    foreach($File in $allFonts)
+    {
+        echo "Installing $File"
+        $CopyFlag = [String]::Format("{0:x}", $CopyOptions);
+        $objFolder.CopyHere($File.fullname,$CopyFlag)
+    }
+    Remove-Item $DesktopPath\Fonts -Recurse -Confirm:$False
+    Write-Host "`nMeslo fonts installed"
+}
+
 function Main {
     Write-Host "============= Pick an option=============="
     Write-Host "`t1. Configure WSL characteristics (Restart)"
     Write-Host "`t2. Update WSL to WSL2"
+    Write-Host "`t3. Install Meslo fonts"
     Write-Host "`t0. Exit"
     Write-Host "========================================================"
     $choice = Read-Host "`nEnter Choice: "
@@ -35,6 +60,10 @@ function Main {
     }
     '2'{
         Update-WSL
+        Main
+    }
+    '3'{
+        Install-Fonts
         Main
     }
     '0'{
@@ -48,8 +77,8 @@ function Set-Admin {
   $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
 if ((Set-Admin) -eq $false)  {
-    $DesktopPath = [Environment]::GetFolderPath("Desktop")
     PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""$DesktopPath\wsl_script.ps1""' -Verb RunAs}"
     exit
 }
