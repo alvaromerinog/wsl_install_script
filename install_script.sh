0e1b4958ca6fc97e41c136e3b26c10bc0899f24c
Alvaro Merino <amerino.informatica@gmail.com>
2021-12-09 21:04:43 +0100

    Add awscli 2 install option

--- install_script.sh
+++ install_script.sh
@@ -58,6 +58,16 @@ function install-fzf() {
     ./install
 }
 
+function install-awscli() {
+    sudo apt update
+    sudo apt install curl zip unzip jq -y
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    sudo rm -r ./awscliv2.zip
+    sudo rm -r ./aws
+}
+
 function fix-apt_pkg() {
     sudo apt remove python3-apt
     sudo apt autoremove -y
@@ -66,42 +76,47 @@ function fix-apt_pkg() {
 }
 
 echo "Select an option:"
-select choice in "Install everything" "Install Python 3.7" "Install Powerlevel10k" "Install VirtualEnvWrapper" "Install Fzf" "Fix APT PKG" "Exit"
+select choice in "Install everything" "Install Python 3.7" "Install VirtualEnvWrapper" "Install Fzf" "Install AWSCLI2"  "Install Powerlevel10k" "Fix APT PKG" "Exit"
 do
     case $choice in
         "Install everything")
             echo "Installing and configuring everything..."
             install-python3.7
-            install-powerlevel10k
             install-virtualenvwrapper
             install-fzf
-        ;;
+            install-awscli
+            install-powerlevel10k
+            ;;
         "Install Python 3.7")
             echo "Installing and configuring Python 3.7..."
             install-python3.7
-        ;;
-        "Install Powerlevel10k")
-            echo "Installing and configuring powerlevel10k..."
-            install-powerlevel10k
-        ;;
+            ;;
         "Install VirtualEnvWrapper")
             echo "Installing and configuring VirtualEnvWrapper..."
             install-virtualenvwrapper
-        ;;
+            ;;
         "Install Fzf")
             echo "Installing and configuring Fzf..."
             install-fzf
-        ;;
+            ;;
+        "Install AWSCLI2")
+            echo "Installing awscli 2 and dependencies..."
+            install-awscli
+            ;;
+        "Install Powerlevel10k")
+            echo "Installing and configuring powerlevel10k..."
+            install-powerlevel10k
+            ;;
         "Fix APT PKG")
             echo "Fixing APT PKG..."
             fix-apt_pkg
-        ;;
+            ;;
         "Exit")
             echo "My job here is done."
             break
-        ;;
+            ;;
         *)
             echo "Unknown option"
-        ;;
+            ;;
     esac
 done
